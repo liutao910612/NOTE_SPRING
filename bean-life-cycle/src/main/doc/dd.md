3f686e34b0cfbc6275837bdf34fd60c94d997445
DESKTOP-FEURCKB\yyt_c <liutao910612@126.com>
2020-12-20 17:03:35 +0800

    Note of merging bean definition

--- bean-life-cycle/src/main/doc/dd.md
+++ bean-life-cycle/src/main/doc/dd.md
@@ -13,10 +13,17 @@
 2. 面向注解BeanDefinition解析
 + AnnotatedBeanDefinitionReader
 
-# Spring Bean注册阶段
+# Spring BeanDefinition注册阶段
 1. BeanDefinition注册接口
 + BeanDefinitionRegistry<br/>
 **总结**注册BeanDefinition是调用的DefaultListableBeanFactory的registerBeanDefinition方法来进行注册，
 其实注册的时候就是往一个key为bean name，value为BeanDefinition的Map里面存值，同时使用一个List来保存插入
 顺序。
+
+# Spring BeanDefinition合并阶段
++ BeanDefinition合并
++ 父子BeanDefinition合并
++ 当前BeanFactory查找
++ 层次性BeaFactory查找<br/>
+**总结**使用BeanDefinition中的parentName属性来进行父类BeanDefinition的查找并进行合并。
 
---
+++ bean-life-cycle/src/main/java/com/kevin/bean/life.cycle/MergedBeanDefinitionDemo.java
@@ -0,0 +1,25 @@
+package com.kevin.bean.life.cycle;
+
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.context.annotation.AnnotatedBeanDefinitionReader;
+
+/**
+ * BeanDefinition合并示例
+ *
+ * @Author:Kevin
+ * @Date:Created in 21:42 2020/12/17
+ */
+public class MergedBeanDefinitionDemo {
+
+    public static void main(String[] args) {
+        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+
+        //基于Java注解的AnnotatedBeanDefinitionReader的实现
+        XmlBeanDefinitionReader beanDefinitionReader = new XmlBeanDefinitionReader(beanFactory);
+        String location = "META-INF/dependency-lookup-context.xml";
+        beanDefinitionReader.loadBeanDefinitions(location);
+        System.out.println(beanFactory.getBean("user"));
+        System.out.println(beanFactory.getBean("superUser"));
+    }
+}

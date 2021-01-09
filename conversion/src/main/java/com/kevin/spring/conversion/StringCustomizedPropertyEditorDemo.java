a5a68384b3578fa893688d6afc7f59d162407fd4
DESKTOP-FEURCKB\yyt_c <liutao910612@126.com>
2021-01-09 16:07:29 +0800

    Complete conversion

--- conversion/src/main/java/com/kevin/spring/conversion/StringCustomizedPropertyEditorDemo.java
+++ conversion/src/main/java/com/kevin/spring/conversion/StringCustomizedPropertyEditorDemo.java
@@ -14,6 +14,15 @@ import java.beans.PropertyEditor;
 public class StringCustomizedPropertyEditorDemo {
     public static void main(String[] args) {
         ConfigurableApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:/META-INF/bean-context.xml");
+
+        //AbstractApplicationContext -> "conversion" ConversionService Bean
+        // -> ConfigurableBeanFactory#setConversionService(ConversionService)
+        // -> AbstractAutowireCapableBeanFactory.instantiateBean
+        // -> AbstractBeanFactory#getConversionService ->
+        // BeanDefinition -> BeanWrapper -> 属性转换（数据来源：PropertiesValues） ->
+        //setPropertyValues(PropertyValues) -> TypeConverter#convertIfNecessary
+        //TypeConverterDelegate#convertIfNecessary -> PropertyEditor or ConversionService
+
         User user = applicationContext.getBean(User.class);
         System.out.println(user);
         applicationContext.close();
